53a48bf92d09225a85f1e6d2e3d71a2542a5a167
John Albritton <jtareb1@att.net>
2015-03-04 14:27:17 -0500

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 ;(function () {
 
+
+
 	'use strict'
 
-var title = samples.title;
-	email = samples.email;
-	textArea = samples.textArea;
-	checkbox = samples.checkbox;
+
 
 
 
@@ -13,6 +12,28 @@ var title = samples.title;
 
 function formBuilder(spec,$el){
   // BEGIN EDITING HERE
+//nested array//
+var samples = [...];
+var newHTML = [];
+//loop through the array
+for(i =0; i <=samples[0].length; i++) {
+	newHTML.push(<span>' + array[i] '</span>);
+});
+$(',el').html(newHTML.join("");
+
+
+
+}
+		
+
+
+
+
+
+
+}
+
+	
 
 
 
